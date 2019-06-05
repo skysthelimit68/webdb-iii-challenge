a23c6755f52a7f36000836d22aeb57217c80e798
Ming Liu <mliu@macpro.home>
2019-06-05 17:10:30 -0400

    all routes and models, mvp

--- api/server.js
+++ api/server.js
@@ -5,11 +5,12 @@ const cohortsRouter = require('../cohorts/cohorts-router.js');
 const studentsRouter = require('../students/students-router.js');
 
 const server = express();
+server.use(express.json());
 
 server.use('/api/cohorts', cohortsRouter);
 server.use('/api/students', studentsRouter);
 
 server.use(helmet());
-server.use(express.json());
+
 
 module.exports = server;
--- cohorts/cohorts-model.js
+++ cohorts/cohorts-model.js
@@ -5,14 +5,54 @@ const db = knex(knexConfig.development);
 
 module.exports = {
     find, // get all cohorts
-    //findById, // get cohort by id
-    //findCohortStudents, // get students of a single cohort
-   // add, // add cohort
-   // update, // make change to cohort
-   // remove // delete cohort
+    findById, // get cohort by id
+    findCohortStudents, // get students of a single cohort
+    add, // add cohort
+    update, // make change to cohort
+    remove // delete cohort
 }
 
 function find() {
     return db('cohorts')
 }
 
+function findById(id) {
+    return db('cohorts')
+    .where( { id })
+    .first()
+}
+
+function add(cohort) {
+    return db('cohorts')
+    .insert(cohort, 'id')
+    .then( ids => {
+        const [id] = ids;
+        return findById(id)
+    })
+}
+
+function update(id, changes) {
+    return db('cohorts')
+    .where({ id })
+    .update(changes)
+    .then( count => {
+        if(count > 0) {
+            return findById(id)
+        } else {
+            return null;
+        }
+    })
+}
+
+function remove(id) {
+    return db('cohorts')
+    .where({ id })
+    .del()
+}
+
+function findCohortStudents(id) {
+    return db('students')
+    .where({ 'cohort_id': id })
+}
+
+//select * from students inner join cohorts on students.cohortId = cohorts.id
